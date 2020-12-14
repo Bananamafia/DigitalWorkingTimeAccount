abbf244655ddb51de816d838a9ea7431601a1695
Bananamafia <maximilian.kiefer@yahoo.de>
2020-12-14 11:23:20 +0100

    Added Day class with first props: StartTime, EndTime, LunchnMin, Balanc

---
+++ ConsoleApplicationWorkingTime/Day.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplicationWorkingTime
+{
+    class Day
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public TimeSpan LunchInMin { get; set; } = new TimeSpan(0, 45, 0);
+        public TimeSpan Balance
+        {
+            get
+            {
+                return EndTime - StartTime - LunchInMin;
+            }
+        }
+    }
+}
