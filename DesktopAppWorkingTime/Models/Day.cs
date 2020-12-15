7ce5cd48ad395e58bd3c53b32e4e94d0839b253a
Bananamafia <maximilian.kiefer@yahoo.de>
2020-12-15 17:49:22 +0100

    Set up day Model

--- DesktopAppWorkingTime/Models/Day.cs
+++ DesktopAppWorkingTime/Models/Day.cs
@@ -7,8 +7,53 @@ namespace DesktopAppWorkingTime.Models
 {
     class Day : INotifyPropertyChanged
     {
-        
-        
+        public DateTime Date { get; set; }
+
+        private DateTime _startTime;
+        public DateTime StartTime
+        {
+            get { return _startTime; }
+            set
+            {
+                _startTime = value;
+                OnPropertyChanged("StartTime");
+                OnPropertyChanged("Balance");
+            }
+        }
+
+        private DateTime _endTime;
+        public DateTime EndTime
+        {
+            get { return _endTime; }
+            set
+            {
+                _endTime = value;
+                OnPropertyChanged("EndTime");
+                OnPropertyChanged("Balance");
+            }
+        }
+
+        private TimeSpan _lunchInMin = new TimeSpan(0, 45, 0);
+        public TimeSpan LunchInMin
+        {
+            get { return _lunchInMin; }
+            set
+            {
+                _lunchInMin = value;
+                OnPropertyChanged("LunchInMin");
+                OnPropertyChanged("Balance");
+            }
+        }
+
+        public TimeSpan Balance
+        {
+            get
+            {
+                return EndTime - StartTime - LunchInMin;
+            }
+        }
+
+
         //---INotifyPropertyChanged---
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged(string property)
