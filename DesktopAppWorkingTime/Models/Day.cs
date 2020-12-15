b39f9544ffd51db3b001c4d2f390879fb5728dcb
Max <63409017+Bananamafia@users.noreply.github.com>
2020-12-15 15:42:01 +0100

    First Setup for WPF Application

---
+++ DesktopAppWorkingTime/Models/Day.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace DesktopAppWorkingTime.Models
+{
+    class Day : INotifyPropertyChanged
+    {
+        
+        
+        //---INotifyPropertyChanged---
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
---
+++ DesktopAppWorkingTime/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesktopAppWorkingTime.ViewModels
+{
+    class MainViewModel
+    {
+    }
+}
