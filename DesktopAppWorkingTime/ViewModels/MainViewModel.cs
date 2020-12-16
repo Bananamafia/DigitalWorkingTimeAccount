4fc0ed1ce8cfd6148836da2b68a0c8ec71fc7bf7
Max <63409017+Bananamafia@users.noreply.github.com>
2020-12-16 13:28:29 +0100

    Record starttime on launch and record end time on sessionending

--- DesktopAppWorkingTime/ViewModels/MainViewModel.cs
+++ DesktopAppWorkingTime/ViewModels/MainViewModel.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DesktopAppWorkingTime.Models;
+using Microsoft.Win32;
 
 namespace DesktopAppWorkingTime.ViewModels
 {
     class MainViewModel
     {
+        public MainViewModel()
+        {
+            LogOperations.RecordStartTime();
+            SystemEvents.SessionEnding += SystemEvents_SessionEnding;
+        }
+
+        private void SystemEvents_SessionEnding(object sender, SessionEndingEventArgs e)
+        {
+            LogOperations.RecordEndTime();
+            System.Windows.Application.Current.Shutdown();
+        }
     }
 }
