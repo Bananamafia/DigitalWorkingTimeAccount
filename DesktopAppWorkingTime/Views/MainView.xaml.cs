109c1ac5f1765113d556a9e8fe0ec0e205c9141b
Bananamafia <maximilian.kiefer@yahoo.de>
2020-12-18 15:56:47 +0100

    added textbox select all when got focus

--- DesktopAppWorkingTime/Views/MainView.xaml.cs
+++ DesktopAppWorkingTime/Views/MainView.xaml.cs
@@ -38,5 +38,14 @@ namespace DesktopAppWorkingTime.Views
 
             base.OnClosing(e);
         }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            var txtControl = sender as TextBox;
+            txtControl.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                txtControl.SelectAll();
+            }));
+        }
     }
 }
